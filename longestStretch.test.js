9664076bcc96f5bfbe9cbd3bc5f227f4727bc310
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-01 19:59:55 -0400

    Add new branch

--- longestStretch.test.js
+++ longestStretch.test.js
@@ -8,6 +8,10 @@ test('Expects an array argument', () => {
     expect(longestStretch("1")).toBe(-1);
 });
 
+test('When argument is [] return should be 0', () => {
+    expect(longestStretch([])).toBe(0);
+});
+
 test('When argument is [1,7,7,3] return should be 2', () => {
     expect(longestStretch([1,7,7,3])).toBe(2);
 });
@@ -16,13 +20,20 @@ test('When argument is [1,7,7,3,9,9,9,4,9] return should be 3', () => {
     expect(longestStretch([1,7,7,3,9,9,9,4,9])).toBe(3);
 });
 
-test('When argument is [] return should be 0', () => {
-    expect(longestStretch([])).toBe(0);
+test('When argument is ["1", "7", "7","3"] return should be 2', () => {
+    expect(longestStretch([1,7,7,3])).toBe(2);
 });
 
+test('When argument is ["1", "7", "7", "3", "9", "9", "9", "4", "9"] return should be 3', () => {
+    expect(longestStretch([1,7,7,3,9,9,9,4,9])).toBe(3);
+});
 
 /*
 Test explanations:
 
+Argument should be an array.
+Argument can be an array with 0 elements.
+Function should correctly return the examples given.
+Function can return the correct integer if the array contains strings.
 
 */ 
