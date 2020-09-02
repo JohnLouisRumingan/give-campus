b5348fab05c367f71fdc9c190a8d20d0e29cc8a6
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-01 20:13:26 -0400

    Change readme

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
 
 ## This repository contains the technical assessment for GiveCampus.
 
+This technical assessment includes both JavaScript and Ruby versions of the algorithms. 
 
-Installation instructions:
+### Installation instructions:
 
 After cloning this repository, install dependencies using ```npm install```.
 You may need to install or update jest with ```npm install jest```.
 Then run tests using ```npm run test```. 
 All unit tests are included under the main folder.
 Each JavaScript file will include notes and assumptions on the algorithms.
+Ruby methods and tests will mirror the same philosophy in JavaScript, but using appropriate methods.
+Ruby explanations will contain notes for differences due to language.
--- longestStretch.test.js
+++ longestStretch.test.js
@@ -20,12 +20,20 @@ test('When argument is [1,7,7,3,9,9,9,4,9] return should be 3', () => {
     expect(longestStretch([1,7,7,3,9,9,9,4,9])).toBe(3);
 });
 
+test('Function should accept array of strings', () => {
+    expect(longestStretch([1,7,7,3,9,9,9,4,9])).toBe(3);
+});
+
 test('When argument is ["1", "7", "7","3"] return should be 2', () => {
     expect(longestStretch([1,7,7,3])).toBe(2);
 });
 
-test('When argument is ["1", "7", "7", "3", "9", "9", "9", "4", "9"] return should be 3', () => {
-    expect(longestStretch([1,7,7,3,9,9,9,4,9])).toBe(3);
+test('Function should differentiate between positive and negative integers', () => {
+    expect(longestStretch([1,-7,7,3])).toBe(1);
+});
+
+test('When argument is [1,7,7,3,9,-9,9,4,9] return should be 2', () => {
+    expect(longestStretch([1,7,7,3,9,9,-9,4,9])).toBe(2);
 });
 
 /*
@@ -35,5 +43,7 @@ Argument should be an array.
 Argument can be an array with 0 elements.
 Function should correctly return the examples given.
 Function can return the correct integer if the array contains strings.
+Function should recognize difference in positive and negative integers.
+Function should also
 
 */ 
