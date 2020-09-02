88b48c1d97287e411803938ed0cb81ebe0fe7aef
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-02 16:11:42 -0400

    Add mostPeople test cases

--- mostPeople.js
+++ mostPeople.js
@@ -1,34 +1,71 @@
 
 function mostPeople(array){
 
-    years = {};
+    const allYears = {};
+    const mostAlive = [];
 
-    if(!Array.isArray(array)) return -1;
+    if(!Array.isArray(array)) return mostAlive;
+    if(array.length <= 0 ) return mostAlive;
 
     for(let person of array){
 
         const birthYear = person[0];
         const deathYear = person[1];
         const lifespan = lifetime(birthYear, deathYear);
+
+        for(let i=0; i<lifespan.length; i++){
+            const year = lifespan[i];
+            allYears[year]? allYears[year] ++ : allYears[year] = 1;
+        }
     }
 
+    const highestPop = allYears[Object.keys(allYears).reduce((a,b) => allYears[a] > allYears[b]? a : b)];
+
+    return Object.keys(allYears).filter(pop => allYears[pop] === highestPop).map( year => parseInt(year, 10)).sort((a,b) => a-b)
 }
 
-function lifetime(start, end, step = 1) {
-    const len = Math.floor((end - start) / step) + 1
-    return Array(len).fill().map((_, idx) => start + (idx * step))
+function lifetime(birth, death, step = 1) {
+    const len = Math.floor((death - birth) / step) + 1;
+    return Array(len).fill().map((_, index) => birth + (index * step))
 }
 
 
-module.exports = mostPeople;
+module.exports = {
+    mostPeople, 
+    lifetime 
+}
 
 /*
+This algorithm runs on O(n) time with O(n) space.
 mostPeople takes in an argument of an array of arrays.
 Expected input is that each array of array contains elements that are integers. 
 Assumptions that these may be strings of numbers are included.
+Similar to longestStretch, if the argument passed in is not an array, it immediately returns a -1.
 
-I want to use a hash table here to create a range of numbers that correspond to years where someone is alive.
+I want to use a hash table here to create a range of keys that correspond to years where someone is alive and values equal to
+the number of people alive during that year.
 Ideally, it would look like: years = {2000: 1, 2001: 4, 2003: 2, 2005: 5}.
-So first, I must 
+An assumption is made here: The person is alive at the start of the year and dies at the end of the year, so they are alive at any 
+point during the year.
+So first, I must take each year and create a range of numbers, inclusive, for them. 
+This is accomplished by a helper function called lifetime.
+This is essentially a spread operator starting from the birth year until death year with an optional step argument of 1.
+Lifespan therefore is an array of years containing one person's lifespan.
+A for loop then adds these years to the allYears hash and records the number of people alive during those years.
+
+Highest pop uses an Object enumerable through all years (keys/properties) of allYears and returns the key of the value of the highest population.
+allYears[key] then assigns the value.
+highestYears takes the properties of an enumerable such as allYears and creates an array. We only want an array of the years with the highest 
+population, so we filter this array with highestPop.
+
+Note that since this is taking the keys/properties of a hashmap in JavaScript, these will be strings. 
+One of my tests actually caught that the return output was an array of strings and not integers.
+We will have to turn these strings into integers so we will use a map method.
+Careful: parseInt requires two arguments every time it is invoked, and simply using .map(parseInt) returns unexpected results!
+To be careful, we are using a one-line arrow notation to keep it succinct and pass the argument of radix 10 to parseInt.
+Because Objects in JavaScript are not guaranteed to have an order, we lastly use .sort() to order the years sequentially.
+At this point, I remove the declaration of highestYears since this variable is the one to be returned.
+
+I have not done strict checking of strings as I have in the longest stretch algorithm and will instead rely on type coersion in JavaScript
 
 */
--- mostPeople.test.js
+++ mostPeople.test.js
@@ -1,11 +1,64 @@
-const mostPeople = require ('./mostPeople')
+const {mostPeople, lifetime } = require ('./mostPeople')
+const testArray = [[1910, 1950], [1900, 1951], [1945, 2000]];
 
 
 test('mostPeople expects an array argument', () => {
-    expect(mostPeople(1)).toBe(-1);
+    expect(mostPeople(1)).toStrictEqual([]);
+    expect(mostPeople("a")).toStrictEqual([]);
+    expect(mostPeople(2.03)).toStrictEqual([]);
+    expect(mostPeople({obj: 2})).toStrictEqual([]);
 });
 
-test('mostPeople expects an array argument', () => {
-    expect(mostPeople("a")).toBe(-1);
+test('mostPeople returns an empty array if argument is an array with zero elements', () => {
+    expect(mostPeople([])).toStrictEqual([]);
 });
 
+test('when lifetime takes in an argument of two integers it returns an array of those integers', () => {
+    expect(lifetime(1900, 1905)).toStrictEqual([1900, 1901, 1902, 1903, 1904, 1905]);
+})
+
+test('lifetime can take in negative numbers for BCE and mixed numbers for BCE and CE', () => {
+    expect(lifetime(-7, -2)).toStrictEqual([-7, -6, -5, -4, -3, -2]);
+    expect(lifetime(-3, 2)).toStrictEqual([-3, -2, -1, 0, 1, 2]);
+})
+
+test('when mostPeople takes in argument [[1910, 1950], [1900, 1951], [1945, 2000]] it returns [1945, 1946, 1947, 1948, 1949, 1950]', () => {
+    expect(mostPeople(testArray)).toStrictEqual([1945, 1946, 1947, 1948, 1949, 1950]);
+})
+
+test('mostPeople can take in negative years for BCE', () => {
+    expect(mostPeople([[-7, -3], [-20, -5], [-4, -2]])).toStrictEqual([-7, -6, -5, -4, -3])
+})
+
+test('mostPeople can take in mixed numbers for BCE and CE with the assumption that a year 0 exists', () => {
+    expect(mostPeople([[-5, 2], [-3, 5], [-20, 10]])).toStrictEqual([-3, -2, -1, 0, 1, 2])
+})
+
+
+
+
+// test('To be or to equal', () => {
+//     const obj = { a: 2 };
+//     expect(obj).toBe(obj);
+//     expect({ a: 2 }).not.toBe({ a: 2 });
+//     expect({ a: 2 }).toEqual({ a: 2 });
+//     expect({ a: 2 }).not.toEqual({ b: 3});
+//     expect({ a: 2 }).toStrictEqual({ a: 2 });
+//     expect({ a: 2 }).toEqual({ a: 2, b: undefined });
+//     expect({ a: 2 })
+//       .not.toStrictEqual({ a: 2, b: undefined });
+// });  
+
+/*
+Test explanations:
+I can use toBe(integer) to return integer values as in longestStretch. However, since the expectation from this function is to return
+an array, returning other data types may cause bugs in other functions. Therefore, the decision was made to change the outputs of 
+incorrect datatypes from an integer to a blank array.
+Since the expected output is an array, expect().toBe has been changed to expect().toStrictEqual due to checking array 
+sparseness and object equality. Object equality is not being checked for, therefore .toBe is not necessarily needed.
+I also want a full comparison of arrays and not just if certain values exist, so I am not using .toContain or .toContainEqual
+
+Function should consider other data types and empty arrays.
+Delegation to helper function should correctly create an array of integers.
+An assumption is made that there is a year 0. 
+*/
